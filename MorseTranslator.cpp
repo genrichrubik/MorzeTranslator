2c81da571648b3c067028b36bd41271b71bb33a1
genrichrubik <genrichrubik@gmail.com>
2023-01-29 20:39:30 +0300

    first commit

---
+++ MorseTranslator.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string morse[26] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--","--.." };
+    string input;
+    cout << "Enter your word:";
+    cin >> input;
+
+    for (int i = 0; i < input.length(); i++) {
+        int index = tolower(input[i]) - 'a';
+
+        cout << morse[index] << "   "; //printing corresponding Morse code for each letter of the given word.
+    }
+
+    return 0;
+}
